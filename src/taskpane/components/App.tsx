d69992b14d91e940bd255a39db4f31987764526e
Leon Letto <leon.letto@gmail.com>
2020-08-20 17:35:24 -0700

    Full Demo App with API for Newsapi.org

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import {Button, ButtonType} from 'office-ui-fabric-react';
+import {Button, ButtonType, TextField} from 'office-ui-fabric-react';
 import Header from './Header';
 import HeroList, {HeroListItem} from './HeroList';
 import Progress from './Progress';
+import axios, {AxiosResponse} from 'axios';
+import * as https from 'https';
+
+const httpsAgent = new https.Agent({keepAlive: true, maxSockets: 40, maxFreeSockets: 10, timeout: 900000});
+
 
 /* global Button, console, Excel, Header, HeroList, HeroListItem, Progress */
 
@@ -13,34 +18,104 @@ export interface AppProps {
 
 export interface AppState {
   listItems: HeroListItem[];
+  currentPage?: string;
+  apiKey: string;
 }
 
 export default class App extends React.Component<AppProps, AppState> {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      listItems: []
+      listItems: [],
+      currentPage: 'home',
+      apiKey: ''
     };
+    this.handleChangeApiKey = this.handleChangeApiKey.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeApiKey(event) {
+    this.setState({apiKey: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   componentDidMount() {
     this.setState({
       listItems: [
+        // {
+        //   icon: 'Ribbon',
+        //   primaryText: 'Achieve more with Office integration'
+        // },
+        // {
+        //   icon: 'Unlock',
+        //   primaryText: 'Unlock features and functionality'
+        // },
+        // {
+        //   icon: 'Design',
+        //   primaryText: 'Create and visualize like a pro'
+        // }
         {
           icon: 'Ribbon',
-          primaryText: 'Achieve more with Office integration'
-        },
-        {
-          icon: 'Unlock',
-          primaryText: 'Unlock features and functionality'
-        },
-        {
-          icon: 'Design',
-          primaryText: 'Create and visualize like a pro'
+          primaryText: 'Try out this Demo App with an API call'
         }
       ]
     });
   }
+
+
+  apiKey = '';
+  getNews = async (apikey: string) => {
+    const columnLetters: string[] = [];
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let x = 0; x < alphabet.length; x++) {
+      for (let y = 0; y < alphabet.length; y++) {
+        if (x === 0) {
+          columnLetters.push(alphabet[y]);
+        } else {
+          columnLetters.push(alphabet[x - 1] + alphabet[y]);
+        }
+      }
+    }
+
+    await axios({
+      url: 'https://' + window.location.host + '/newsapitopheadlines' + '?country=us&category=business&apikey=' + apikey,
+      method: 'get',
+      timeout: 90000,
+      httpsAgent: httpsAgent,
+      headers: {
+        'Content-Type': `application/x-www-form-urlencoded`,
+        Accept: 'application/json',
+      },
+    }).then(async (response: AxiosResponse) => {
+      let models = [];
+
+      models.push(['Source', 'Title', 'Description', 'PublishedAt', 'Content', 'Url', 'UrlToImage']);
+      await response.data.articles.map(async (data) => {
+        models.push([data.source.name, data.title, data.description, data.publishedAt, data.content, data.url, data.urlToImage]);
+      });
+      await Excel.run(async (context: Excel.RequestContext) => {
+        let sheetTitle = 'TopNews';
+        context.workbook.worksheets.getItemOrNullObject(sheetTitle).delete();
+        const sheet = context.workbook.worksheets.add(sheetTitle);
+        let dataWidth = models[0].length;
+        let dataLength = models.length;
+        for (let i = 0; i < dataLength; i + 1000 > dataLength ? i = dataLength : i = i + 1000) {
+          var range = sheet.getRange('A' + (i + 1) + ':' + columnLetters[dataWidth - 1] + (i + 1000 > dataLength ? dataLength : i + 1000));
+          range.values = models.slice(i, (i + 1000 > dataLength ? dataLength : i + 1000));
+          sheet.activate();
+          await context.sync();
+        }
+
+        sheet.getUsedRange().getEntireColumn().format.autofitColumns();
+        sheet.getUsedRange().getEntireRow().format.autofitRows();
+        sheet.activate();
+      });
+    });
+
+  };
 
   click = async () => {
     try {
@@ -67,6 +142,46 @@ export default class App extends React.Component<AppProps, AppState> {
   render() {
     const {title, isOfficeInitialized} = this.props;
 
+    // Set the body of the page based on state.
+    let body;
+
+    if (this.state.currentPage === 'home') {
+      body = <HeroList message="Get some News!" items={this.state.listItems}>
+        {/*<p className="ms-font-l">*/}
+        {/*  Modify the source files, then click <b>Run</b>.*/}
+        {/*</p>*/}
+        {/*<Button*/}
+        {/*  className="ms-welcome__action"*/}
+        {/*  buttonType={ButtonType.hero}*/}
+        {/*  iconProps={{iconName: 'ChevronRight'}}*/}
+        {/*  onClick={this.click}*/}
+        {/*>*/}
+        {/*  Run*/}
+        {/*</Button>*/}
+        <form onSubmit={this.handleSubmit}>
+          <TextField
+            label="ApiKey"
+            className='ms-welcome__formitems'
+            autoCapitalize='none'
+            type=''
+            value={this.state.apiKey}
+            onChange={this.handleChangeApiKey}
+            placeholder='Enter NewsApi ApiKey'
+          /> <Button
+          className="ms-welcome__action"
+          buttonType={ButtonType.hero}
+          iconProps={{iconName: 'ChevronRight'}}
+          onClick={() => {
+            this.getNews(this.state.apiKey);
+          }}
+        >
+          Get News
+        </Button>
+        </form>
+
+      </HeroList>;
+    }
+
     if (!isOfficeInitialized) {
       return (
         <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body."/>
@@ -76,19 +191,20 @@ export default class App extends React.Component<AppProps, AppState> {
     return (
       <div className="ms-welcome">
         <Header logo="assets/logo-filled.png" title={this.props.title} message="Welcome"/>
-        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
-          <p className="ms-font-l">
-            Modify the source files, then click <b>Run</b>.
-          </p>
-          <Button
-            className="ms-welcome__action"
-            buttonType={ButtonType.hero}
-            iconProps={{iconName: 'ChevronRight'}}
-            onClick={this.click}
-          >
-            Run
-          </Button>
-        </HeroList>
+        {body}
+        {/*<HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>*/}
+        {/*  <p className="ms-font-l">*/}
+        {/*    Modify the source files, then click <b>Run</b>.*/}
+        {/*  </p>*/}
+        {/*  <Button*/}
+        {/*    className="ms-welcome__action"*/}
+        {/*    buttonType={ButtonType.hero}*/}
+        {/*    iconProps={{iconName: 'ChevronRight'}}*/}
+        {/*    onClick={this.click}*/}
+        {/*  >*/}
+        {/*    Run*/}
+        {/*  </Button>*/}
+        {/*</HeroList>*/}
       </div>
     );
   }
--- src/taskpane/components/HeroList.tsx
+++ src/taskpane/components/HeroList.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 
 export interface HeroListItem {
   icon: string;
@@ -12,18 +12,26 @@ export interface HeroListProps {
 
 export default class HeroList extends React.Component<HeroListProps> {
   render() {
-    const { children, items, message } = this.props;
+    const {children /*,items*/, message} = this.props;
 
-    const listItems = items.map((item, index) => (
-      <li className="ms-ListItem" key={index}>
-        <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
-        <span className="ms-font-m ms-fontColor-neutralPrimary">{item.primaryText}</span>
-      </li>
-    ));
+    // const listItems = items.map((item, index) => (
+    //   <li className="ms-ListItem" key={index}>
+    //     <i className={`ms-Icon ms-Icon--${item.icon}`}></i>
+    //     <span className="ms-font-m ms-fontColor-neutralPrimary">{item.primaryText}</span>
+    //   </li>
+    // ));
     return (
       <main className="ms-welcome__main">
         <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">{message}</h2>
-        <ul className="ms-List ms-welcome__features ms-u-slideUpIn10">{listItems}</ul>
+        <ul className="ms-List ms-welcome__features ms-u-slideUpIn10">
+          {/*{listItems}*/}
+          <li className="ms-ListItem"><i className={`ms-Icon ms-Icon--Ribbon`}/><span
+            className="ms-font-m ms-fontColor-neutralPrimary">Go to <a
+            href='https://newsapi.org/'>Newsapi.org</a> and get yourself an API Key</span></li>
+          <li className="ms-ListItem"><i className={`ms-Icon ms-Icon--Unlock`}/><span
+            className="ms-font-m ms-fontColor-neutralPrimary">Put the API Key in below and push Get News to get the Top Headlines.</span>
+          </li>
+        </ul>
         {children}
       </main>
     );
